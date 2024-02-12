a14d2aa45afd41b5154f3187f056837334eb2e8c
simaokane <simadrien@gmail.com>
2024-02-12 01:41:47 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import { useEffect, useState } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
+
+const url = "https://course-api.com/react-tabs-project";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [tabs, setTabs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  const fetchTabs = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const tabs = await response.json();
+      setLoading(false);
+      setTabs(tabs);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTabs();
+  }, []);
+
+  if (loading) {
+    return (
+      <section className="section loading">
+        <h1>Loading...</h1>
+      </section>
+    );
+  }
+
+  const { company, dates, duties, title } = tabs[value];
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>experience</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="jobs-center">
+        {/* btn container */}
+        <div className="btn-container">
+          {tabs.map((item, index) => {
+            return (
+              <button
+                key={item.id}
+                onClick={() => setValue(index)}
+                className={`job-btn ${index === value && "active-btn"}`}
+              >
+                {item.company}
+              </button>
+            );
+          })}
+        </div>
+
+        <article className="job-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className="job-desc">
+                <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
+                <p>{duty}</p>
+              </div>
+            );
+          })}
+        </article>
+      </div>
+      <button type="button" className="btn">
+        more info
+      </button>
+    </section>
+  );
+}
+
+export default App;
